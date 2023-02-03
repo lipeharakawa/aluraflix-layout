cd49adedb2928bc3bea6ffa96b1c7ccd492e1ac6
lipeharakawa <felipeharakawa01@gmail.com>
2023-02-03 01:51:09 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center"> Aluraflix - Layout </h1>
+
+<p align="center">
+Imersão Dev é um evento exclusivo e gratuito, promovido pela Alura para ensino de tecnologias WEB. <br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>     |    
+  <a href="#-projeto">Projeto</a>     |    
+  <a href="#-layout">Layout</a>     |    
+  <a href="#-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="Licença" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Foto projeto Aluraflix - Layout" src=".github/preview.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+- CodePen
+
+## 💻 Projeto
+
+O Aluraflix - Layout permite o cadastramento de dados (URL da imagem) por meio de uma array através do código (certifique-se que o formato da imagem cadastrada seja PNG, JPG ou relacionadas) e é exibido o resultado na tela do navegador.
+
+- [ Visite o projeto online ]()
+
+## 🔖 Layout
+
+Você pode visualizar o layout base do projeto através de [ DESSE LINK ](https://codepen.io/imersao-dev/pen/JjBvBbe). É necessário ter conta no [ CodePen ](https://codepen.io/accounts/signup/user/free) para acessá-lo.
+
+## ✅ Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com 🧡 por Felipe Seidi Harakawa, durante o evento Imersão Dev da Alura. [ Visite o site! ](https://www.alura.com.br)
---
+++ script.js
@@ -0,0 +1,25 @@
+var listaFilmes = ["https://br.web.img2.acsta.net/medias/nmedia/18/89/56/94/20055685.jpg",
+                    "https://musicart.xboxlive.com/7/d7fc4500-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+                    "https://br.web.img2.acsta.net/pictures/22/09/23/20/14/0505071.jpg",
+                    "https://lumiere-a.akamaihd.net/v1/images/unnamed_13_75a3ebb1.jpeg?region=0%2C0%2C356%2C512",
+                    "https://br.web.img2.acsta.net/pictures/19/07/10/20/01/2331295.jpg",
+                    "https://www.clickriomafra.com.br/emacite/wp-content/uploads/2022/04/Animais-Fant%C3%A1sticos-3-ser%C3%A1-exibido-no-Cineplus-Emacite-confira-a-programa%C3%A7%C3%A3o-1.jpg",
+                    "https://br.web.img2.acsta.net/pictures/18/11/08/14/45/3423559.jpg",
+                    "https://sm.ign.com/ign_br/screenshot/default/pokemon-first_32qq.jpg",
+                    "https://s2.glbimg.com/co1HagXAnL1RH0f4Vc3FMNqpC-4=/362x536/https://s2.glbimg.com/alwqepi-sVFMrdvPKeMkosBvH4E=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2022/4/n/chM3O1TiAxPfWhtDB6lA/6222869-poster.jpg",
+                    "http://cdn.fstatic.com/public/articles/files/2014/09/8116898a-9270-4646-bdfb-47745beeccff.jpg",
+                    "https://upload.wikimedia.org/wikipedia/pt/8/8e/Divergent.png",
+                    "https://extra.globo.com/incoming/17656610-8fa-6f8/w533h800/cartaz.jpg",
+                    "http://www.cinesystem.com.br/filmes/images/poster/avatar-o-caminho-da-agua.jpg",
+                    "https://upload.wikimedia.org/wikipedia/pt/e/e6/Xmen_apocalypse_ver18.jpg"
+                ]
+
+//listaFilmes[0] = "https://br.web.img2.acsta.net/medias/nmedia/18/89/56/94/20055685.jpg";
+//listaFilmes[1] = "https://musicart.xboxlive.com/7/d7fc4500-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080";
+//listaFilmes[2] = "https://br.web.img2.acsta.net/pictures/22/09/23/20/14/0505071.jpg";
+
+for (var i=0; i<listaFilmes.length; i++) {
+    
+    document.write("<img src=" + listaFilmes[i] + ">");
+    
+}
